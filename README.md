4479f2e58c292ed1610a94d7521c279292a1ae4d
Dan Jarvis <danwakeem@gmail.com>
2018-07-06 12:54:30 -0500

    More obvious endpoint names

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 In the `common/models/sample.js` file there is a remote method defined. It is supposed to return a non root custom model type in an array.
 
 When you view the API explorer it shows the custom model type as a blank Object rather than the Model object. 
+
+
+> See the `GET /sample/fail/{id}` endpoint to view the error
--- common/models/sample.js
+++ common/models/sample.js
@@ -3,17 +3,31 @@
 module.exports = function(Sample) {
   Sample.sharedClass._methods.forEach(obj => Sample.disableRemoteMethodByName(obj.name));
 
-  Sample.remoteMethod('testById', {
+  Sample.remoteMethod('incorrect', {
     accepts: [
       { arg: 'id', type: 'string', required: true },
     ],
-    http: { path: '/remote/test/:id', verb: 'get' },
+    http: { path: '/fail/:id', verb: 'get' },
     returns: { arg: 'test', type: [Sample] },
   });
 
-  Sample.testById = (id) => {
+  Sample.remoteMethod('correct', {
+    accepts: [
+      { arg: 'id', type: 'string', required: true },
+    ],
+    http: { path: '/pass/:id', verb: 'get' },
+    returns: { arg: 'test', type: [Sample], root: true },
+  });
+
+
+  Sample.incorrect = (id) => {
     const filter = { where: { id } };
-    return Test.find(id);
+    return Sample.find(filter);
   };
+
+  Sample.incorrect = (id) => {
+    const filter = { where: { id } };
+    return Sample.find(filter);
+  }
 
 };
